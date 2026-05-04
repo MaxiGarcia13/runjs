f0c5688667d8b351aef058a9b8fcf57059e64d38
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-04 15:50:49 +0200

    Implement Monaco Editor in Editor component
    - Integrated Monaco Editor for enhanced code editing experience.
    - Configured editor settings including language, theme, and layout options.
    - Utilized useRef and useEffect hooks for editor instance management.
    - Updated component structure to accept optional className prop for styling.

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,3 +1,41 @@
-export function Editor() {
-  return <div>Editor</div>
+import { editor } from 'monaco-editor';
+import { useEffect, useRef } from 'react';
+import { cn } from '@/utils/classes';
+
+interface EditorProps {
+  className?: string;
+}
+
+export function Editor({ className }: EditorProps) {
+  const editorRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (editorRef.current) {
+      const instance = editor.create(editorRef.current, {
+        value: 'console.log("Hello, world!");',
+        language: 'javascript',
+        theme: 'vs-dark',
+        fontSize: 15,
+        minimap: {
+          enabled: false,
+
+        },
+
+        automaticLayout: true,
+        fixedOverflowWidgets: true,
+        scrollBeyondLastLine: false,
+        roundedSelection: false,
+      });
+
+      return () => {
+        instance.dispose();
+      };
+    }
+  }, []);
+
+  return (
+    <section className={cn('box-border h-full min-h-0 w-full overflow-hidden', className)}>
+      <div ref={editorRef} className="h-full w-full" />
+    </section>
+  );
 }
