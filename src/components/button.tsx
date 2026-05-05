d4045298b3c66c515358a7c6c0b300950cae8635
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-05 15:46:23 +0200

    Refactor Button and ShareButton components for improved functionality and clarity
    - Updated Button component to accept additional props and extended its interface for better type safety.
    - Modified ShareButton to use LinkIcon instead of ShareIcon and updated the button text to "Copy link".
    - Integrated toast notification to confirm link copying action, enhancing user feedback.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,6 +1,14 @@
-export function Button({ children, onClick }: { children: React.ReactNode; onClick: () => void }) {
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function Button({ children, ...props }: ButtonProps) {
   return (
-    <button className="border border-gray-700 p-2 rounded-md flex items-center gap-2 cursor-pointer hover:bg-gray-700 transition-colors" onClick={onClick}>
+    <button
+      className="border border-gray-700 p-2 rounded-md text-sm flex items-center gap-2 cursor-pointer hover:bg-gray-700 transition-colors"
+      {...props}
+    >
       {children}
     </button>
   );
--- src/components/share-button.tsx
+++ src/components/share-button.tsx
@@ -1,16 +1,21 @@
-import { ShareIcon } from '@/assets/icons/share';
+import { LinkIcon } from '@/assets/icons/link';
+import { useToast } from '@/components/toast';
 import { Button } from './button';
 
 export function ShareButton() {
+  const { showToast } = useToast();
+
   const handleShare = () => {
     const url = window.location.href;
     navigator.clipboard.writeText(url);
+
+    showToast('Link copied to clipboard');
   };
 
   return (
     <Button onClick={handleShare}>
-      <ShareIcon className="size-4" />
-      <span>Share</span>
+      <LinkIcon className="size-4" />
+      <span>Copy link</span>
     </Button>
   );
 }
