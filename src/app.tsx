130a332005e3463c152accf464472fb63114225a
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-05 15:45:07 +0200

    Wrap App component with ToastProvider and add Footer for enhanced layout and notification management

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,13 @@ import { Editor } from '@/components/editor';
 import { Header } from '@/components/header';
 import { Preview } from '@/components/preview';
 import { ResizablePanel } from '@/components/resizable-panel';
+import { ToastProvider } from '@/components/toast';
 import { isMobile } from '@/utils/device';
+import { Footer } from './components/footer';
 
 export function App() {
   return (
-    <>
+    <ToastProvider>
       <Header />
       <ResizablePanel
         className="flex-1 min-h-0 overflow-hidden bg-gray-800 rounded-md mx-2 mb-2 border border-gray-700"
@@ -14,6 +16,7 @@ export function App() {
         leftContent={<Editor className="p-2" />}
         rightContent={<Preview className="p-2" />}
       />
-    </>
+      <Footer />
+    </ToastProvider>
   );
 }
