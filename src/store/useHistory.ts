5130026090e87a3edda669e0fa0eda6f2eaee701
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-07 22:04:00 +0200

    feat: Enhance history management store with additional functionalities for editing and removing history entries

--- src/store/useHistory.ts
+++ src/store/useHistory.ts
@@ -1,39 +1,79 @@
 import { create } from 'zustand';
 
+export interface History {
+  payload: string;
+  createdAt: number;
+  label: string;
+}
+
 interface HistoryStore {
-  history: Array<{ payload: string; createdAt: number; label: string }>;
-  push: (payload: string) => void;
+  histories: Array<History>;
   clear: () => void;
+  remove: (createdAt: number) => void;
+  addHistory: (payload: string) => void;
+  editHistory: (editedHistory: Partial<History>) => void;
 }
 
 const HISTORY_STORAGE_KEY = 'runjs-history';
 
 export const useHistory = create<HistoryStore>((set) => ({
-  history: getHistoryValue(),
-  push: (payload: string, label?: string) => {
-    set((state) => {
-      const history = [
-        ...state.history,
-        {
-          payload,
-          label: label ?? payload,
-          createdAt: Date.now(),
-        },
-      ];
-
-      localStorage.setItem(HISTORY_STORAGE_KEY, JSON.stringify(history));
-
-      return { history };
-    });
-  },
+  histories: getHistoryValue(),
   clear: () => {
     localStorage.removeItem(HISTORY_STORAGE_KEY);
-    set({ history: [] });
+    set({ histories: [] });
+  },
+  remove: (createdAt: number) => {
+    set((state) => {
+      const histories = state.histories
+        .filter((history) => history.createdAt !== createdAt);
+
+      localStorage.setItem(HISTORY_STORAGE_KEY, JSON.stringify(histories));
+      return { histories };
+    });
+  },
+  addHistory: (payload: string, label?: string) => {
+    set((state) => {
+      const exist = state.histories.find((history) => history.payload === payload);
+
+      if (exist) {
+        return { histories: state.histories };
+      }
+
+      const histories = sortHistories([
+        ...state.histories,
+        {
+          payload,
+          label,
+          createdAt: Date.now(),
+        },
+      ]);
+
+      localStorage.setItem(HISTORY_STORAGE_KEY, JSON.stringify(histories));
+
+      return { histories };
+    });
+  },
+  editHistory: (editedHistory: Partial<History>) => {
+    set((state) => {
+      const histories = state.histories
+        .map((history) =>
+          history.createdAt === editedHistory.createdAt
+            ? { ...history, ...editedHistory }
+            : history,
+        );
+
+      localStorage.setItem(HISTORY_STORAGE_KEY, JSON.stringify(histories));
+      return { histories };
+    });
   },
 }));
 
 function getHistoryValue() {
-  return localStorage.getItem(HISTORY_STORAGE_KEY)
+  return sortHistories(localStorage.getItem(HISTORY_STORAGE_KEY)
     ? JSON.parse(localStorage.getItem(HISTORY_STORAGE_KEY))
-    : [];
+    : []);
+}
+
+function sortHistories(histories: Array<History>) {
+  return histories.sort((a, b) => b.createdAt - a.createdAt);
 }
