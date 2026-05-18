3017d7d276ae01f088e01143a336631f1f435e1e
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-18 23:44:34 +0200

    feat: Extend Output interface to include optional call site information for enhanced debugging

--- src/components/preview/types.ts
+++ src/components/preview/types.ts
@@ -6,10 +6,16 @@ export type Variant
     | 'perf-log'
     | 'test-log';
 
+export interface CallSite {
+  line: number;
+  column: number;
+}
+
 export interface Output {
   id: string;
   type: Variant;
   content?: OutputContent;
+  callSite?: CallSite;
 }
 
 export type OutputContent = string | OutputTestContent;
